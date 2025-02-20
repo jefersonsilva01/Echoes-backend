f81bd1680d0eb40e1bd17409753823adccc829f5
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2025-02-20 11:17:07 -0300

    Update backend

--- config/index.js
+++ config/index.js
@@ -16,6 +16,13 @@ const allowedOrigins = [
 module.exports = (app) => {
   app.set("trust proxy", 1);
 
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+  });
+
   app.use(
     cors({
       credentials: true,
